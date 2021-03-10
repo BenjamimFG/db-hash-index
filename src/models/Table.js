fcc9e94438e9e31dbfa9b59307fb2ce1a3a9f2a1
BenjamimFG <ben.FreitasG@gmail.com>
2021-03-09 23:26:19 -0300

    refactor(src): move all table info to the Table class
    move pages, buckets lists and respective initializer functions from script.js to models/Table.js
    move hashing function from script.js to models/Table.js
    move logic to set null input parameters from script.js to models/Table.js

--- src/models/Table.js
+++ src/models/Table.js
@@ -1,14 +1,35 @@
 class Table {
-  constructor() {
+  /**
+   * @param {number} pageSize 
+   * @param {number} totalPages 
+   * @param {number} bucketSize 
+   * @param {number} totalBuckets 
+   * @param {number} initialRecordsSize initial size of data that will be inserted
+   */
+  constructor(pageSize, totalPages, bucketSize, totalBuckets, initialRecordsSize) {
     /**
      * @type {Tuple[]}
      */
     this.tuples = [];
+    /**
+     * @type {Page[]}
+     */
+    this.pages = [];
+    /**
+     * @type {Bucket[]}
+     */
+    this.buckets = [];
+    
+    this.pageSize = pageSize;
+    this.totalPages = totalPages;
+    this.bucketSize = bucketSize;
+    this.totalBuckets = totalBuckets;
+
+    this._setNullParameters(initialRecordsSize);
+    this._initializePages();
+    this._initializeBuckets();
+
     this.metadata = {
-      pageSize: undefined,
-      totalPages: undefined,
-      bucketSize: undefined,
-      totalBuckets: undefined,
       collisions: 0,
       overflows: 0
     }
@@ -19,5 +40,100 @@ class Table {
    */
   addTuple(tuple) {
     this.tuples.push(tuple);
+    const tuplePage = this._addTupleToPages(tuple);
+    this._addDataToBucket(tuple.key, tuplePage);
+  }
+
+  /**
+   * Generates a hash from a `key`
+   * @private
+   * @param {String} key Key to generate hash from
+   * @return {Number} hash generated from key
+   */
+  _hashingFunction(key) {
+    let sum = 0;
+    let charArray = key.split('');
+
+    charArray.forEach(char => {
+      sum += char.charCodeAt(0);
+    });
+
+    return sum % this.totalBuckets;
+  }
+
+  /**
+   * @private
+   * @param {Tuple} tuple 
+   * @returns {Page}
+   */
+  _addTupleToPages(tuple) {
+    for (let i = 0; i < this.pages.length; ++i) {
+      if (this.pages[i].tuples.length < this.pages[i].maxSize) {
+        this.pages[i].addTuple(tuple);
+        return this.pages[i];
+      }
+    }
+
+    throw Error('Total pages size exceeded');
+  }
+
+  /**
+   * @private
+   */
+  _addDataToBucket(key, page) {
+    const hash = this._hashingFunction(key);
+
+    if (hash < 0 || hash >= this.buckets.length) {
+      alert('Invalid key hash value');
+      throw new Error('Invalid key hash value');
+    }
+    
+    const bucket = this.buckets[hash];
+    bucket.addData(new BucketData(key, page));
+  }
+
+  /**
+   * @private
+   */
+  _initializePages() {
+    for (let i = 0; i < this.totalPages; ++i)
+      this.pages.push(new Page(i, this.pageSize));
+  }
+
+  /**
+   * @private
+   */
+  _initializeBuckets() {
+    for (let i = 0; i < this.totalBuckets; ++i)
+      this.buckets.push(new Bucket(i, this.bucketSize));
+  }
+
+  /**
+   * @private
+   * @param {number} initialRecordsSize 
+   */
+  _setNullParameters(initialRecordsSize) {
+    if ((this.pageSize !== null && this.totalPages !== null) || (this.pageSize === null && this.totalPages === null)) {
+      alert('Please select either a page size OR total number of pages.');
+      throw new Error('Please select either a page size OR total number of pages.');
+    }
+    if ((this.bucketSize !== null && this.totalBuckets !== null) || (this.bucketSize === null && this.totalBuckets === null)) {
+      alert('Please select either a bucket size OR total number of buckets.');
+      throw new Error('Please select either a bucket size OR total number of buckets.');
+    }
+
+    if (this.pageSize !== null) {
+      this.totalPages = Math.ceil(initialRecordsSize / this.pageSize);
+    }
+    else if (this.totalPages !== null) {
+      this.pageSize = Math.ceil(initialRecordsSize / this.totalPages);
+    }
+  
+    if (this.bucketSize !== null) {
+      this.totalBuckets = Math.ceil(initialRecordsSize / this.bucketSize);
+    }
+    else if (this.totalBuckets !== null) {
+      this.bucketSize = Math.ceil(initialRecordsSize / this.totalBuckets);
+    }
   }
 }
--- src/script.js
+++ src/script.js
@@ -1,21 +1,10 @@
-const table = new Table()
 /**
- * @type {Page[]}
+ * @type {Table}
  */
-
-table.metadata.pageSize = Number.parseInt(document.querySelector('#page-size').value) || null;
-table.metadata.totalPages = Number.parseInt(document.querySelector('#total-pages').value) || null;
-table.metadata.bucketSize = Number.parseInt(document.querySelector('#bucket-size').value) || null;
-table.metadata.totalBuckets = Number.parseInt(document.querySelector('#total-buckets').value) || null;
-
-const pages = []
-/**
- * @type {Bucket[]}
- */
-const buckets = []
+let table;
 
 /**
- * Reads file from an HTML input to start the   
+ * Reads file from an HTML input to start the table insertion
  * @param {HTMLInputElement} fileInput
  */
 function readFile(fileInput) {
@@ -28,82 +17,59 @@ function readFile(fileInput) {
     return;
   }
 
-  if ((table.metadata.pageSize !== null && table.metadata.totalPages !== null) || (table.metadata.pageSize === null && table.metadata.totalPages === null)) {
-    alert('Please select either a page size OR total number of pages.');
-    return;
-  }
-
-  /**
-   * @type {Tuple[]}
-   */
-  // let tuples = [];
-
   file.text().then(text => {
-    text.split('\n').forEach(word => {
+    const words =  text.split('\n');
+
+    table = new Table(
+      Number.parseInt(document.querySelector('#page-size').value) || null,
+      Number.parseInt(document.querySelector('#total-pages').value) || null,
+      Number.parseInt(document.querySelector('#bucket-size').value) || null,
+      Number.parseInt(document.querySelector('#total-buckets').value) || null,
+      words.length
+    );
+
+    words.forEach(word => {
       word = word.trim();
-      const hash = hashingFunction(word);
-      const cur_tuple = new Tuple(hash, word);
+      // const hash = hashingFunction(word);
+      const currentTuple = new Tuple(word, word);
       
-      // tuples.push(cur_tuple);
-      table.addTuple(cur_tuple);
+      table.addTuple(currentTuple);
     })
   }).then(_ => {
-    if ((table.metadata.bucketSize !== null && table.metadata.totalBuckets !== null) || (table.metadata.bucketSize === null && table.metadata.totalBuckets === null)) {
-      alert('Please select either a bucket size OR total number of buckets.');
-      return;
-    }
-  
-    if (table.metadata.pageSize !== null) {
-      table.metadata.totalPages = Math.ceil(table.tuples.length / table.metadata.pageSize);
-    }
-    else if (table.metadata.totalPages !== null) {
-      table.metadata.pageSize = Math.ceil(table.tuples.length / table.metadata.totalPages);
-    }
-  
-    if (table.metadata.bucketSize !== null) {
-      table.metadata.totalBuckets = Math.ceil(table.tuples.length / table.metadata.bucketSize);
-    }
-    else if (table.metadata.totalBuckets !== null) {
-      table.metadata.bucketSize = Math.ceil(table.tuples.length / table.metadata.totalBuckets);
-    }
-  
     debugLogState();
-  
-    initializePages(table.metadata.totalPages);
-    initializeBuckets(255);
+    logPages();
+    logBuckets();
+  });
+}
+
+function debugLogState() {
+  console.log("total tuples: " + table.tuples.length, "\npage size: " + table.pageSize, "\ntotal pages: " + table.totalPages, "\nbucket size: " + table.bucketSize, "\ntotal buckets: " + table.totalBuckets);
+}
+
+function logPages() {
+  table.pages.forEach(page => {
+    console.log(`Page ${page.id} (${page.tuples.length} tuples):`);
+    page.tuples.forEach(tuple => {
+      console.log(`  key: ${tuple.key}, data: ${tuple.data}`);
+    });
+  });
+}
+
+function logBuckets() {
+  table.buckets.forEach(bucket => {
+    logBucket(bucket);
   });
 }
 
 /**
- * Initializes the pages list with `numberOfPages` pages
- * @param {Number} numberOfPages Number of pages to be created
+ * @param {Bucket} bucket 
  */
-function initializePages(numberOfPages) {
-  for (let i = 0; i < numberOfPages; ++i)
-    pages.push(new Page(table.metadata.pageSize));
-}
-
-/**
- * Initializes the buckets list with `numberOfBuckets` buckets
- * @param {Number} numberOfBuckets Number of buckets to be created
- */
-function initializeBuckets(numberOfBuckets) {
-  for (let i = 0; i < numberOfBuckets; ++i)
-    buckets.push(new Bucket(i));
-}
-
-/**
- * Generates a hash from a `key`
- * @param {String} key Key to generate hash from
- * @return {Number} hash generated from key
- */
-function hashingFunction(key) {
-  let sum = 0;
-  key.split('').reduce((total, char) => total += char.charCodeAt(0), sum);
-
-  return sum % table.metadata.totalBuckets;
-}
-
-function debugLogState() {
-  console.log("total tuples: " + table.tuples.length, "\npage size: " + table.metadata.pageSize, "\ntotal pages: " + table.metadata.totalPages, "\nbucket size: " + table.metadata.bucketSize, "\ntotal buckets: " + table.metadata.totalBuckets);
+function logBucket(bucket, level = 0) {
+  console.log('  '.repeat(level) + `bucket ${level > 0 ? `overflow ${level - 1}` : bucket.id} (${bucket.getTotalSize()} entries):`);
+  bucket.data.forEach(data => {
+    console.log('  '.repeat(level + 1) + `key: ${data.key}, page: ${data.page.id}`);
+  });
+  if (bucket.bucketOverflow !== null) {
+    logBucket(bucket.bucketOverflow, ++level);
+  }
 }
